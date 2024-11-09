d27ef41e2113b31ff8b1219b7eb27e7ab01d4ff6
Alexander <SaintDeveloperGames@gmail.com>
2024-11-09 15:04:54 +0300

    tg_game

---
+++ nginx/html/config.php
@@ -0,0 +1,12 @@
+<?php
+    $config = array(
+        'db' => array(
+            'server' => '192.168.0.103',
+            'username' => 'root',
+            'password' => 'password',
+            'name' => 'db_tg'
+        )
+    );
+    include('db.php');
+?>
+
